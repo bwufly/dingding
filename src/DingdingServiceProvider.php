c935e04ec784c1df437c79ea8be8904a9c4cc1fa
wufly <wfxykzd@163.com>
2020-03-03 16:23:11 +0800

    dingding配置加载

---
+++ src/DingdingServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class DingdingServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //publish
+        $this->publishes([
+            __DIR__ . '/config/dingding.php' => config_path('dingding.php'),
+        ]);
+    }
+}
